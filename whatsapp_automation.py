5739dd40b8a848ceea304deb018d81f1cd564845
eloadev <eloamello126@gmail.com>
2022-12-29 21:24:48 -0300

    feat: added the automation that access whatsapp and send the message

--- whatsapp_automation.py
+++ whatsapp_automation.py
@@ -1,20 +1,30 @@
+from time import sleep
+
+from selenium import webdriver
+from selenium.webdriver import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
 
 
-class AutomacaoZap:
-    def __init__(self, message):
-        self.message = message
+class AutomationZap:
 
-    def envia_mensagem(self, message):
-        number = '+5522981474624'
+    @staticmethod
+    def send_message(message, contact):
+        driver = webdriver.Chrome()
+        driver.get("https://web.whatsapp.com/")
 
-        Webdriver driver = ChromeDriver()
-        try:
-            pywhatkit.sendwhatmsg_instantly(number, "teste", 6)
-            pywhatkit.sendwhatmsg_instantly(number, "elo", 6)
-            print("Success")
-        except Exception:
-            print("Error")
+        search_box = WebDriverWait(driver, 50).until(lambda driver: driver.find_element(By.XPATH, """//*[
+        @id="side"]/div[1]/div/div/div[2]/div/div[2]"""))
+        search_box.send_keys(contact)
 
+        selected_contact = driver.find_element(By.XPATH, "//span[@title='" + contact + "']")
+        selected_contact.click()
+        message_input_box = driver.find_element(By.XPATH, """//*[@id="main"]/footer/div[1]/div/span[2]/div/div[
+        2]/div[1]/div/div[1]""")
 
-teste = AutomacaoZap("aaa")
-teste.envia_mensagem("aaa")
+        for x in range(len(message)):
+            message_input_box.clear()
+            message_input_box.send_keys(message[x] + Keys.ENTER)
+
+        sleep(60)
+        driver.close()
