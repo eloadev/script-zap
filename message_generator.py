73f8b596dcff3d7cd19e5a14bc37ff4b49885347
Eloa Mello <eloamello126@gmail.com>
2023-01-15 18:32:37 -0300

    refactor: rename variables

--- message_generator.py
+++ message_generator.py
@@ -4,20 +4,20 @@ import json
 class Message:
     def define_message(self):
         with open("scriptMovies.json", encoding='utf-8') as scripts:
-            data = json.load(scripts)
+            json_scripts = json.load(scripts)
 
-        for x in data:
-            print(x['id'], ": ", x['title'])
+        for movie in json_scripts:
+            print(movie['id'], ": ", movie['title'])
 
         option = int(input("Type the corresponding number for the movie script:  "))
 
-        movie = None
+        chosen_movie = None
 
-        for y in data:
-            if y['id'] == option:
-                movie = y
+        for movie in json_scripts:
+            if movie['id'] == option:
+                chosen_movie = movie
                 break
 
-        split_script = movie['script'].split("\n")
+        split_script = chosen_movie['script'].split("\n")
 
         return split_script
